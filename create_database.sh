4f0eaf001824de0b14a605be2a9562b7d567f4f4
Brian Findlay <bfindlay@fastmail.com>
2016-05-13 15:54:57 -0400

    Remove zsh dependency

--- create_database.sh
+++ create_database.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /bin/bash
 
 # Directories
 source_dir="source"
@@ -12,14 +12,14 @@ createdb ${dbname}
 echo "Database created."
 
 # Execute SQL commands from files to build database
-for file (${source_dir}/*.sql); do
+for file in ${source_dir}/*.sql; do
     echo "Executing ${file}..."
-    psql --file=${file} --quiet --dbname=${dbname}
+    psql --file ${file} --quiet --dbname ${dbname}
 done
 
 # Create backup
 echo "Backing up to ${dbname}.dump..."
-pg_dump --format=custom ${dbname} > ${dbname}.dump
+pg_dump --no-owner --format custom --compress 9 ${dbname} > ${dbname}.dump
 echo "Backup complete."
 
 echo "Done."
